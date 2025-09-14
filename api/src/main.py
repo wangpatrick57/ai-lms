9b2b6d2433bbc0928d4a4c7e176f0491841dd1cf
Patrick Wang <wang.patrick57@gmail.com>
2025-09-14 15:45:42 -0700

    pex

--- api/src/main.py
+++ api/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+import os
 
 app = FastAPI()
 
@@ -7,5 +8,7 @@ app = FastAPI()
 def hello_world():
     return {"message": "Hello, world!"}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+def main():
+    # Render injects the PORT envvar.
+    port = int(os.environ.get("PORT", "8000"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
