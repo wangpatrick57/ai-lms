e363c345f835b19d32f3a7eae98c8e6128b56720
Patrick Wang <wang.patrick57@gmail.com>
2025-09-13 19:30:24 -0700

    [Misc] Basic question component (#2)

---
+++ src/App/App.tsx
@@ -0,0 +1,12 @@
+import Question from "../Question/Question.tsx";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <Question />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Question/Question.tsx
@@ -0,0 +1,7 @@
+import styles from "./Question.module.css";
+
+function Question() {
+  return <div className={styles.question}>hi</div>;
+}
+
+export default Question;
